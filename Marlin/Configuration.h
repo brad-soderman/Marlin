f7eb2f5d0b8c07b20ae59fb186533a6ee4ee31d5
Brad <brad.soderman@gmail.com>
2021-07-16 11:40:16 -0600

    switch to EZR Struder - updated esteps and board

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -891,7 +891,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 396, 138 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 396, 93 }
 
 /**
  * Default Max Feed Rate (mm/s)
